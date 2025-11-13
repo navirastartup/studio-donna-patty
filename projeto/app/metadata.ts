408de591afbea6a3e7049ef6fd3cfcc8164161e2
donnapatricia <agendastudiodonnapatty@gmail.com>
2025-11-13 13:49:22 -0300

    Delete projeto directory

--- projeto/app/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Studio Donna Patty",
-  description: "Seu cuidado com a sua beleza é o nosso maior compromisso.",
-};
